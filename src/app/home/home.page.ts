0e8f67788c8a7c6c58dcc0c91a35386fe7cf9c0b
fumihiro.sunada <fumihiro.sunada@bigtreetc.com>
2019-09-12 16:28:03 +0900

    list/3

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {WordpressService} from '../services/wordpress.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  providers: [WordpressService]
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
+  title: string;
+  items: any[];
+  loading: boolean;
 
-  constructor() {}
+  constructor(public wordpressService: WordpressService) {
+    // for debug log
+    console.log('HomePage constructor() was celled!');
+    this.title = '記事一覧';
+  }
 
+  ngOnInit() {
+    // for debug log
+    console.log('HomePage ngOnInit() was celled!');
+    this.loading = true;
+    this.wordpressService.getPosts().subscribe(data => {
+      this.title = '記事一覧（' + data.posts.length + '件）';
+      this.items = data.posts;
+      this.loading = false;
+    });
+  }
+
+  ngOnDestroy() {
+    // for debug log
+    console.log('HomePage ngOnDestroy() was celled!');
+  }
 }
