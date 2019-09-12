6ea69dad55de2bece8fb90ed6f6c0fa987d0c35d
fumihiro.sunada <fumihiro.sunada@bigtreetc.com>
2019-09-12 16:23:41 +0900

    list/2

--- src/app/services/wordpress.service.ts
+++ src/app/services/wordpress.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WordpressService {
 
-  constructor() { }
+  mainUrl = 'https://public-api.wordpress.com/rest/v1.1/sites/ionicjp.wordpress.com/';
+
+  constructor(private http: HttpClient) { }
+
+  // 投稿一覧取得
+  public getPosts(): any {
+    return this.http.get(this.mainUrl + 'posts/');
+  }
 }
