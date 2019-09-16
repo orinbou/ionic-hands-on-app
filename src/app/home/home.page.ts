8084ad4b663c20ec96eac1326fc3561ad0cd9236
fumihiro.sunada <fumihiro.sunada@bigtreetc.com>
2019-09-16 11:25:07 +0900

    camera/2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {WordpressService} from '../services/wordpress.service';
+import {Camera, CameraOptions} from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-home',
@@ -11,8 +12,9 @@ export class HomePage implements OnInit, OnDestroy {
   title: string;
   items: any[];
   loading: boolean;
+  currentImage: any;
 
-  constructor(public wordpressService: WordpressService) {
+  constructor(public wordpressService: WordpressService, private camera: Camera) {
     // for debug log
     console.log('HomePage constructor() was celled!');
     this.title = '記事一覧';
@@ -32,5 +34,24 @@ export class HomePage implements OnInit, OnDestroy {
   ngOnDestroy() {
     // for debug log
     console.log('HomePage ngOnDestroy() was celled!');
+  }
+
+  // カメラ撮影
+  takePicture() {
+    // for debug log
+    console.log('HomePage takePicture() was celled!');
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.currentImage = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+      console.log('Camera issue:' + err);
+    });
   }
 }
